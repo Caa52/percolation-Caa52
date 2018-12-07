3b28daa310b2238e8b49ba51038c0532038c7144
caa52 <collins.abanda@duke.edu>
2018-12-06 20:57:23 -0500

    percolate update

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -12,45 +12,49 @@ public class PercolationBFS extends PercolationDFSFast {
 	protected void dfs(int row, int col) {
 		// out of bounds?
 		// int size = 0;
-		myGrid[row][col] = row * myGrid.length + col;
+		// myGrid[row][col] = row * myGrid.length + col;
 
 		Queue<Integer> qp = new LinkedList<Integer>();
 		myGrid[row][col] = FULL;
 		// size++; // count pixel
-		qp.add((row*myGrid.length)+col);
+		qp.add((row * myGrid.length) + col);
 
 		while (qp.size() != 0) {
-			Integer p = qp.remove();
+			int p = qp.remove();
 			int deqRow = p / myGrid.length;
 			int deqCol = p % myGrid.length;
 
 			// for(int k=0; k < rowDelta.length; k++){
-			//row*myGrid.length + col
 
-			if (inBounds((deqRow) - 1, (deqCol))) {
-				if (isOpen((deqRow) - 1, (deqCol)) && !isFull((deqRow) - 1, (deqCol)))
+			if (inBounds(deqRow - 1, deqCol)) {
+				if (isOpen(deqRow - 1, deqCol) && !isFull(deqRow - 1, deqCol)) {
+					myGrid[deqRow - 1][deqCol] = FULL;
+					qp.add((deqRow - 1) * myGrid.length + deqCol);
+				}
+
+			}
+
+			if (inBounds(deqRow + 1, deqCol)) {
+				if (isOpen(deqRow + 1, deqCol) && !isFull(deqRow + 1, deqCol)) {
 					myGrid[(deqRow) - 1][deqCol] = FULL;
-				qp.add((row*myGrid.length)+col);
+					qp.add((deqRow + 1) * myGrid.length + deqCol);
+				}
 			}
 
-			if (inBounds((deqRow) + 1, (deqCol))) {
-				if (isOpen((deqRow) + 1, (deqCol)) && !isFull((deqRow) + 1, (deqCol)))
-					myGrid[(deqRow) - 1][deqCol] = FULL;
-				qp.add((row*myGrid.length)+col);
+			if (inBounds(deqRow, deqCol + 1)) {
+				if (isOpen(deqRow, deqCol + 1) && !isFull(deqRow, deqCol + 1)) {
+					myGrid[deqRow][deqCol + 1] = FULL;
+					// size++;
+					qp.add(deqRow * myGrid.length + deqCol + 1);
+				}
 			}
 
-			if (inBounds((deqRow), (deqCol) + 1)) {
-				if (isOpen((deqRow), (deqCol) + 1) && !isFull((deqRow), (deqCol) + 1))
-					myGrid[(deqRow)][(deqCol) + 1] = FULL;
-				// size++;
-				qp.add((row*myGrid.length)+col);
-			}
-
-			if (inBounds((deqRow), (deqCol) - 1)) {
-				if (isOpen((deqRow), (deqCol) - 1) && !isFull((deqRow), (deqCol) - 1))
-					myGrid[(deqRow)][(deqCol) - 1] = FULL;
-				// size++;
-				qp.add((row*myGrid.length)+col);
+			if (inBounds(deqRow, deqCol - 1)) {
+				if (isOpen(deqRow, deqCol - 1) && !isFull(deqRow, deqCol - 1)) {
+					myGrid[deqRow][deqCol - 1] = FULL;
+					// size++;
+					qp.add(deqRow * myGrid.length + deqCol - 1);
+				}
 			}
 
 		}
---
+++ src/PercolationUF.java
@@ -0,0 +1,108 @@
+
+public class PercolationUF implements IPercolate {
+
+	IUnionFind myFinder;
+	int myOpenCount;
+	private final int VTOP;
+	private final int VBOTTOM;
+	boolean[][] myGrid;
+
+	public PercolationUF(int size, IUnionFind finder) {
+
+		myGrid = new boolean[size][size];
+		// myGrid[size][size] = false;
+
+		myOpenCount = 0;
+		myFinder = new QuickUWPC(size * size + 2);
+		VTOP = size * size;
+		VBOTTOM = size * size + 1;
+
+	}
+	
+	boolean inBounds(int row, int col) {
+		// TODO Auto-generated method stub
+		if (row < 0 || row >= myGrid.length)
+			return false;
+		if (col < 0 || col >= myGrid.length)
+			return false;
+		return true;
+
+	}
+
+
+	@Override
+	public void open(int row, int col) {
+		// TODO Auto-generated method stub
+		if (!inBounds(row, col)) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		if (!isOpen(row, col)) {
+			myGrid[row][col] = true;
+
+			if (row == 0) {
+				myFinder.union(row * myGrid.length + col, VTOP);
+
+			}
+
+			if (row == myGrid.length - 1) {
+				myFinder.union(row * myGrid.length + col, VBOTTOM);
+			}
+			if (inBounds(row, col + 1) && isOpen(row, col + 1)) {
+				myFinder.union(row * myGrid.length + (col + 1), row * myGrid.length + col);
+
+			}
+			if (inBounds(row, col - 1) && isOpen(row, col - 1)) {
+				myFinder.union(row * myGrid.length + (col - 1), row * myGrid.length + col);
+
+			}
+			if (inBounds(row + 1, col) && isOpen(row + 1, col)) {
+				myFinder.union(row * myGrid.length + col, (row + 1) * myGrid.length + col);
+
+			}
+			if (inBounds(row - 1, col) && isOpen(row - 1, col)) {
+				myFinder.union(row * myGrid.length + col, (row - 1) * myGrid.length + col);
+
+			}
+		}
+	}
+
+	@Override
+	public boolean isOpen(int row, int col) {
+		// TODO Auto-generated method stub
+		if (!inBounds(row, col))
+			throw new IndexOutOfBoundsException();
+		if (myGrid[row][col] == true)
+			return true;
+		return false;
+	}
+
+	@Override
+	public boolean isFull(int row, int col) {
+		// TODO Auto-generated method stub
+		if (!inBounds(row, col))
+			throw new IndexOutOfBoundsException();
+		if (myFinder.connected((row * myGrid.length + col), VTOP))
+			;
+		return true;
+
+	}
+
+	@Override
+	public boolean percolates() {
+		// TODO Auto-generated method stub
+		if (myFinder.connected(VTOP, VBOTTOM)) 
+			return true;
+		return false;
+
+	}
+
+	@Override
+	public int numberOfOpenSites() {
+		// TODO Auto-generated method stub
+		return myOpenCount;
+	}
+
+	
+
+}
